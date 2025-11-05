92f840ee0a50cf54a3c07e94e30ee5eb8a677bab
Wiktor Murawski <wiktor0204@gmail.com>
2025-11-05 02:16:15 +0100

    Initial commit

---
+++ ElectronsAndProtons_CPU/main.cpp
@@ -0,0 +1,46 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World CPU1234!\n");
+
+    GLFWwindow* window;
+
+    ///* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    ///* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    ///* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    ///* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
