84e44ede94ae8778de6ab03fca1e670e468708ec
Wiktor Murawski <wiktor0204@gmail.com>
2025-11-06 19:56:34 +0100

    Particles start

--- ElectronsAndProtons_CPU/main.cpp
+++ ElectronsAndProtons_CPU/main.cpp
@@ -9,21 +9,69 @@
 
 #define TITLE "ElectronsAndProtons_CPU"
 
-#define HEIGHT 720
-#define WIDTH 1280
+#define WIDTH 800
+#define HEIGHT 600
 
 #define MINVAL 0.0f
 #define MAXVAL 1.0f
 
-// Convert [min, max] value → RGB (blue -> red)
-static void value_to_color(float value, float min, float max, unsigned char* r, unsigned char* g, unsigned char* b)
+struct particles_t
 {
-    float t = (value - min) / (max - min);
+    double* x;
+    double* y;
+    double* vx;
+    double* vy;
+    double* q;
+    int count;
+
+    ~particles_t()
+    {
+        delete[] this->x;
+        delete[] this->y;
+        delete[] this->vx;
+        delete[] this->vy;
+        delete[] this->q;
+        this->count = 0;
+    }
+};
+
+// Convert [min, max] value -> RGB (blue -> white -> red)
+static void value_to_color(double value, double min, double max, unsigned char* r, unsigned char* g, unsigned char* b)
+{
+    double t = (value - min) / (max - min);
     if (t < 0.0f) t = 0.0f;
     if (t > 1.0f) t = 1.0f;
-    *r = (unsigned char)(t * 255);
-    *g = 0;
-    *b = (unsigned char)((1.0f - t) * 255);
+
+    if (t < 0.5f) {
+        double f = t / 0.5f;
+        *r = (unsigned char)(f * 255);
+        *g = (unsigned char)(f * 255);
+        *b = 255;
+    }
+    else {
+        double f = (t - 0.5f) / 0.5f;
+        *r = 255;
+        *g = (unsigned char)((1.0f - f) * 255);
+        *b = (unsigned char)((1.0f - f) * 255);
+    }
+}
+
+static void generateRandomParticles(particles_t* particles, int count, double min_x, double max_x, double min_y, double max_y, double min_vx, double max_vx, double min_vy, double max_vy, double electronCharge, double protonCharge)
+{
+    particles->count = count;
+    particles->x = new double[count];
+    particles->y = new double[count];
+    particles->vx = new double[count];
+    particles->vy = new double[count];
+    particles->q = new double[count];
+
+    for (int i = 0; i < count; i++) {
+        particles->x[i] = min_x + (double)rand() / (double)RAND_MAX * (max_x - min_x);
+        particles->y[i] = min_y + (double)rand() / (double)RAND_MAX * (max_y - min_y);
+        particles->vx[i] = min_vx + (double)rand() / (double)RAND_MAX * (max_vx - min_vx);
+        particles->vy[i] = min_vy + (double)rand() / (double)RAND_MAX * (max_vy - min_vy);
+        particles->q[i] = (rand() % 2) ? electronCharge : protonCharge;
+    }
 }
 
 int main(void)
@@ -32,9 +80,6 @@ int main(void)
 
     int width = WIDTH;
     int height = HEIGHT;
-
-    float* values = new float[width * height];
-    unsigned char* pixels = new unsigned char[width * height * 3];
 
     if (!glfwInit()) {
         return -1;
@@ -48,6 +93,7 @@ int main(void)
     }
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
     glOrtho(0, 1, 0, 1, -1, 1);
 
     GLuint texture;
@@ -58,25 +104,50 @@ int main(void)
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
 
+    double* field = new double[width * height];
+    unsigned char* pixels = new unsigned char[width * height * 3];
+    particles_t particles;
+    //int count = 10'000;
+    int count = 100;
+    generateRandomParticles(&particles, count, 0, width, 0, height, -5, +5, -5, +5, -1.0, 1.0);
+
     double prevTime = glfwGetTime();
     int frames = 0;
     double fps = 0.0;
     while (!glfwWindowShouldClose(window))
     {
         // --- Calculations ---
-        // Generate random data
+        // Calculate field values
         for (int i = 0; i < width * height; i++) {
-            //values[i] = MINVAL + (float)rand() / (float)RAND_MAX * (MAXVAL - MINVAL);
-            values[i] = i / (float)(width * height);
+            double value = 0;
+            for (int j = 0; j < particles.count; j++)
+            {
+                int x = i % width;
+                int y = i / width;
+                double dist2 = (x - particles.x[j]) * (x - particles.x[j]) + (y - particles.y[j]) * (y - particles.y[j]);
+                value += particles.q[j] / (dist2 + 1e-6);
+            }
+            field[i] = value;
         }
 
         // Map to colors
         for (int i = 0; i < width * height; i++) {
             unsigned char r, g, b;
-            value_to_color(values[i], MINVAL, MAXVAL, &r, &g, &b);
+            value_to_color(field[i], -0.1, 0.1, &r, &g, &b);
             pixels[i * 3 + 0] = r;
             pixels[i * 3 + 1] = g;
             pixels[i * 3 + 2] = b;
+        }
+
+        // Draw particles
+        for (int i = 0; i < particles.count; i++)
+        {
+            int x = (int)particles.x[i];
+            int y = (int)particles.y[i];
+            int idx = (y * width + x) * 3;
+            pixels[idx] = 0;     // R
+            pixels[idx + 1] = 0; // G
+            pixels[idx + 2] = 0;   // B
         }
 
         // --- Display ---
@@ -113,8 +184,8 @@ int main(void)
         glfwPollEvents();
     }
 
-    free(values);
-    free(pixels);
+    delete[] field;
+    delete[] pixels;
     glfwTerminate();
     return 0;
 }
