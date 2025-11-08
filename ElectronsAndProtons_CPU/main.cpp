347010a6783f448ece641705835a81594dfde092
Wiktor Murawski <wiktor0204@gmail.com>
2025-11-09 00:03:19 +0100

    Refactoring and cleanup

--- ElectronsAndProtons_CPU/main.cpp
+++ ElectronsAndProtons_CPU/main.cpp
@@ -1,9 +1,5 @@
-﻿//#ifdef _WIN32
-//#include <windows.h>
-//#endif
-
+﻿#include <GLFW/glfw3.h>
 #include <algorithm>
-#include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
@@ -19,7 +15,7 @@
 
 #define EPS 1e-6
 
-#define K 1e4 // stała elektrostatyczna, wprost proporcjonalna do siły z jaką cząstki odzdziałują na siebie
+#define K 1e3 // stała elektrostatyczna, wprost proporcjonalna do siły z jaką cząstki odzdziałują na siebie
 #define MASS_PROTON 1.0
 #define MASS_ELECTRON 1.0
 #define CHARGE_PROTON 1.0
@@ -52,6 +48,7 @@ static void value_to_color(double value, double min, double max, unsigned char* 
     }
 }
 
+// Zapełnia strukturę nieruchomymi cząstkami
 static void generateStationaryParticles(particles_t* particles, double min_x, double max_x, double min_y, double max_y)
 {
     for (int i = 0; i < particles->count; i++) {
@@ -65,6 +62,7 @@ static void generateStationaryParticles(particles_t* particles, double min_x, do
     }
 }
 
+// Zapełnia strukturę losowo poruszającymi się cząstkami
 static void generateRandomlyMovingParticles(particles_t* particles, double min_x, double max_x, double min_y, double max_y, double min_vx, double max_vx, double min_vy, double max_vy)
 {
     for (int i = 0; i < particles->count; i++) {
@@ -78,7 +76,15 @@ static void generateRandomlyMovingParticles(particles_t* particles, double min_x
     }
 }
 
-int main(void)
+int setupWindowAndOpenGLContext(GLFWwindow** window, GLuint* texture, int width, int height);
+void calculateElectricField(double* field, int width, int height, i2xy_t* i2xy, particles_t* particles);
+void mapFieldValuesToPixels(double* field, int width, int height, uint8_t* pixels);
+void drawParticles(particles_t* particles, uint8_t* pixels, int width, int height);
+void updateAccelerations(particles_t* particles);
+void moveParticles(particles_t* particles, double dt);
+void displayTextureFromPixels(GLuint* texture, uint8_t* pixels, int width, int height);
+
+int main(int argc, char** argv)
 {
     srand((unsigned int)time(nullptr));
 
@@ -86,148 +92,52 @@ int main(void)
     int height = HEIGHT;
     int count = PARTICLE_COUNT;
 
-    // Ustawienie okna i OpenGL
-    if (!glfwInit()) {
-        return -1;
-    }
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    GLFWwindow* window = glfwCreateWindow(width, height, TITLE, nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(0);
-    glOrtho(0, 1, 0, 1, -1, 1);
-
+    GLFWwindow* window;
     GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    // Koniec ustawiania OpenGL
+    if(setupWindowAndOpenGLContext(&window, &texture, width, height) < 0){
+        printf("Error during window setup\n");
+        return -1;
+    }
 
     i2xy_t i2xy(width, height);
     double* field = new double[width * height];
-    unsigned char* pixels = new unsigned char[width * height * 3];
+    uint8_t* pixels = new uint8_t[width * height * 3];
     particles_t particles(count);
     generateRandomlyMovingParticles(&particles, 0, width, 0, height, -2, +2, -2, +2);
 
-    int frames = 0;
-    double prevTime = glfwGetTime();
+    uint64_t frames = 0;
+    double frameRefreshInterval = 0.2;
     double prevFrameTime = glfwGetTime();
+    double prevTime = prevFrameTime;
+
     while (!glfwWindowShouldClose(window))
     {
-        double currentTime = glfwGetTime();
-        double dt = currentTime - prevFrameTime;
-        prevFrameTime = currentTime;
+        double currentFrameTime = glfwGetTime();
+        double dt = currentFrameTime - prevFrameTime;
+        prevFrameTime = currentFrameTime;
         dt = std::min(dt, 0.05);
 
         // --- Obliczenia ---
-        for (int i = 0; i < width * height; i++) {
-            // Obliczanie wartości nateżenie pola elektrycznego w punkcie [N/C]
-            double value = 0;
-            for (int j = 0; j < particles.count; j++)
-            {
-                //int x = i % width;
-                //int y = i / width;
-                int x = i2xy.x[i];
-                int y = i2xy.y[i];
-                double dx = x - particles.x[j];
-                double dy = y - particles.y[j];
-                double dist2 = dx * dx + dy * dy;
-                value += particles.q[j] / dist2;
-            }
-            field[i] = value;
-
-            // Mapowanie wartości natężenia pola elektrycznego na gradient
-            unsigned char r, g, b;
-            value_to_color(field[i], CHARGE_ELECTRON, CHARGE_PROTON, &r, &g, &b);
-            pixels[i * 3 + 0] = r;
-            pixels[i * 3 + 1] = g;
-            pixels[i * 3 + 2] = b;
-        }
-
-        // Nanoszenie cząstek na obraz
-        for (int i = 0; i < particles.count; i++)
-        {
-            int x = (int)particles.x[i];
-            int y = (int)particles.y[i];
-            int idx = (y * width + x) * 3;
-            pixels[idx] = 0;     // R
-            pixels[idx + 1] = 0; // G
-            pixels[idx + 2] = 0; // B
-        }
-
-        // Obliczanie przyspieszeń wynikających z pola elektrycznego
-        for (int i = 0; i < particles.count; i++) {
-            double ax = 0.0;
-            double ay = 0.0;
-            for (int j = 0; j < particles.count; j++) {
-                if (i == j) continue;
-
-                double dx = particles.x[i] - particles.x[j];
-                double dy = particles.y[i] - particles.y[j];
-                double dist2 = dx * dx + dy * dy + EPS;
-                double invDist = 1.0 / sqrt(dist2);
-                double invDist3 = invDist * invDist * invDist;
-
-                double f = K * particles.q[i] * particles.q[j] * invDist3;
-                ax += f * dx;
-                ay += f * dy;
-            }
-
-            particles.ax[i] = ax;
-            particles.ay[i] = ay;
-        }
-
-        //double dt = 0.5; // time step
-        for (int i = 0; i < particles.count; i++) {
-            particles.vx[i] += particles.ax[i] * dt;
-            particles.vy[i] += particles.ay[i] * dt;
-
-            particles.x[i] += particles.vx[i] * dt;
-            particles.y[i] += particles.vy[i] * dt;
-
-            // Odbicia od ścian
-            if (particles.x[i] < 0) { particles.x[i] = 0; particles.vx[i] *= -1; }
-            if (particles.x[i] > WIDTH - 1) { particles.x[i] = WIDTH - 1; particles.vx[i] *= -1; }
-            if (particles.y[i] < 0) { particles.y[i] = 0; particles.vy[i] *= -1; }
-            if (particles.y[i] > HEIGHT - 1) { particles.y[i] = HEIGHT - 1; particles.vy[i] *= -1; }
-        }
-
+        calculateElectricField(field, width, height, &i2xy, &particles);
+        mapFieldValuesToPixels(field, width, height, pixels);
+        drawParticles(&particles, pixels, width, height);
+        
         // --- Wyświetlanie ---
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-
-        glClear(GL_COLOR_BUFFER_BIT);
-        glEnable(GL_TEXTURE_2D);
-        glBegin(GL_QUADS);
-        glTexCoord2f(0, 1); glVertex2f(0, 0);
-        glTexCoord2f(1, 1); glVertex2f(1, 0);
-        glTexCoord2f(1, 0); glVertex2f(1, 1);
-        glTexCoord2f(0, 0); glVertex2f(0, 1);
-        glEnd();
-        glDisable(GL_TEXTURE_2D);
+        displayTextureFromPixels(&texture, pixels, width, height); 
+        
+        // --- Obliczenia ---
+        updateAccelerations(&particles);
+        moveParticles(&particles, dt);
 
         // --- Wyznaczanie FPS ---
         frames++;
-        double t = glfwGetTime();
-        double interval = 0.2;
-        //printf("Frametime: %.2fms\n", (t - pTime)*1000);
-        if (t - prevTime >= interval) {
-            double fps = frames / (t - prevTime);
+        if (currentFrameTime - prevTime >= frameRefreshInterval) {
+            double fps = frames / (currentFrameTime - prevTime);
             char title[128];
             snprintf(title, sizeof(title), "%s - FPS: %.1f", TITLE, fps);
-            //printf("FPS: %.1f\n", fps);
-            //printf("Frametime: %.1fms\n", (t - prevTime) / frames * 1000);
             glfwSetWindowTitle(window, title);
             frames = 0;
-            prevTime = t;
+            prevTime = currentFrameTime;
         }
 
         glfwSwapBuffers(window);
@@ -238,4 +148,141 @@ int main(void)
     delete[] field;
     delete[] pixels;
     return 0;
+}
+
+// Ustawienie okna i OpenGL
+int setupWindowAndOpenGLContext(GLFWwindow** window, GLuint* texture, int width, int height){
+    if (!glfwInit()) {
+        return -1;
+    }
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    *window = glfwCreateWindow(width, height, TITLE, nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(*window);
+    glfwSwapInterval(0);
+    glOrtho(0, 1, 0, 1, -1, 1);
+
+    glGenTextures(1, texture);
+    glBindTexture(GL_TEXTURE_2D, *texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    return 0;
+}
+
+void displayTextureFromPixels(GLuint* texture, uint8_t* pixels, int width, int height){
+    glBindTexture(GL_TEXTURE_2D, *texture);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 1); glVertex2f(0, 0);
+    glTexCoord2f(1, 1); glVertex2f(1, 0);
+    glTexCoord2f(1, 0); glVertex2f(1, 1);
+    glTexCoord2f(0, 0); glVertex2f(0, 1);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+}
+
+// Obliczanie natężenia pola elektrycznego
+void calculateElectricField(double* field, int width, int height, i2xy_t* i2xy, particles_t* particles){
+    for (int i = 0; i < width * height; i++) {
+        double value = 0;
+        for (int j = 0; j < particles->count; j++)
+        {
+            int x = i2xy->x[i];
+            int y = i2xy->y[i];
+            double dx = x - particles->x[j];
+            double dy = y - particles->y[j];
+            double dist2 = dx * dx + dy * dy;
+            value += particles->q[j] / dist2;
+        }
+        field[i] = value;
+    }
+}
+
+// Mapowanie wartości natężenia pola elektrycznego na gradient
+void mapFieldValuesToPixels(double* field, int width, int height, uint8_t* pixels){
+    for (int i = 0; i < width * height; i++) {
+        unsigned char r, g, b;
+        value_to_color(field[i], CHARGE_ELECTRON, CHARGE_PROTON, &r, &g, &b);
+        pixels[i * 3 + 0] = r;
+        pixels[i * 3 + 1] = g;
+        pixels[i * 3 + 2] = b;
+    }
+}
+
+// Nanoszenie cząstek do rysowanej tablicy pixeli
+void drawParticles(particles_t* particles, uint8_t* pixels, int width, int height){
+    for (int i = 0; i < particles->count; i++)
+    {
+        int x = (int)particles->x[i];
+        int y = (int)particles->y[i];
+        int idx = (y * width + x) * 3;
+        pixels[idx] = 0;     // R
+        pixels[idx + 1] = 0; // G
+        pixels[idx + 2] = 0; // B
+    }
+}
+
+// Obliczanie przyspieszeń wynikających z pola elektrycznego
+void updateAccelerations(particles_t* particles){
+    for (int i = 0; i < particles->count; i++) {
+        double ax = 0.0;
+        double ay = 0.0;
+        for (int j = 0; j < particles->count; j++) {
+            if (i == j) continue;
+
+            double dx = particles->x[i] - particles->x[j];
+            double dy = particles->y[i] - particles->y[j];
+            double dist2 = dx * dx + dy * dy + EPS;
+            double invDist = 1.0 / sqrt(dist2);
+            double invDist3 = invDist * invDist * invDist;
+
+            double f = K * particles->q[i] * particles->q[j] * invDist3;
+            ax += f * dx;
+            ay += f * dy;
+        }
+
+        particles->ax[i] = ax;
+        particles->ay[i] = ay;
+    }
+}
+
+// Zmiania prędkości i położenia cząstek na podstawie przyspieszeń
+void moveParticles(particles_t* particles, double dt){
+    for (int i = 0; i < particles->count; i++) {
+        // Zmiana prędkości
+        particles->vx[i] += particles->ax[i] * dt;
+        particles->vy[i] += particles->ay[i] * dt;
+
+        // Zmiana położenia
+        particles->x[i] += particles->vx[i] * dt;
+        particles->y[i] += particles->vy[i] * dt;
+
+        // Odbicia
+        if (particles->x[i] < 0) { 
+            particles->x[i] = 0; 
+            particles->vx[i] *= -1; 
+        }
+        if (particles->x[i] > WIDTH - 1) { 
+            particles->x[i] = WIDTH - 1; 
+            particles->vx[i] *= -1; 
+        }
+        if (particles->y[i] < 0) { 
+            particles->y[i] = 0; 
+            particles->vy[i] *= -1; 
+        }
+        if (particles->y[i] > HEIGHT - 1) { 
+            particles->y[i] = HEIGHT - 1; 
+            particles->vy[i] *= -1; 
+        }
+    }
 }
