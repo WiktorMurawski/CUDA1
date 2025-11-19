c40ba043a6b57605e8d7c22e975489311d55ebcb
Wiktor Murawski <wiktor0204@gmail.com>
2025-11-19 22:51:16 +0100

    damping

--- ElectronsAndProtons_CPU/main.cpp
+++ ElectronsAndProtons_CPU/main.cpp
@@ -19,6 +19,8 @@
 #define DEFAULT_WIDTH 600
 #define DEFAULT_HEIGHT 600
 #define DEFAULT_PARTICLE_COUNT 100
+#define DEFAULT_DAMPING 0.999
+#define DEFAULT_DRAG 0.8
 
 #define TITLE "ElectronsAndProtons_CPU"
 
@@ -29,7 +31,7 @@ static void value_to_color(double value, double min, double max, unsigned char* 
     
     if (t < 0.5) {
         double f = 2 * t;
-        double ratio = 1 - sqrt(1 - f);
+        double ratio = 1 - sqrt(1 - f); 
         unsigned char byte = ratio * 255;
         *r = byte;
         *g = byte;
@@ -71,27 +73,29 @@ static void generateRandomlyMovingParticles(particles_t* particles, double min_x
     }
 }
 
+void parseInputArgs(int argc, char** argv, int* width, int* height, int* count, double* damping);
 int setupWindowAndOpenGLContext(GLFWwindow** window, GLuint* texture, int width, int height);
 void calculateElectricField(double* field, int width, int height, i2xy_t* i2xy, particles_t* particles);
 void mapFieldValuesToPixels(double* field, int width, int height, uint8_t* pixels);
 void drawParticles(particles_t* particles, uint8_t* pixels, int width, int height);
 void updateAccelerations(particles_t* particles);
-void moveParticles(particles_t* particles, int width, int height, double dt);
+void moveParticles(particles_t* particles, int width, int height, double dt, double damping);
 void displayTextureFromPixels(GLuint* texture, uint8_t* pixels, int width, int height);
-void parseInputArgs(int argc, char** argv, int* width, int* height, int* count);
 
 int main(int argc, char** argv)
 {
     // seed dla rand() wykorzystywanego do generacji losowych cząstek
     srand((unsigned int)time(nullptr));
 
+    // Domyślne parametry symulacji
     int width = DEFAULT_WIDTH;
     int height = DEFAULT_HEIGHT;
     int count = DEFAULT_PARTICLE_COUNT;
+    double damping = DEFAULT_DAMPING;
 
     // Parsowanie argumentów wejściowych
-    parseInputArgs(argc, argv, &width, &height, &count);
-    printf("Ustawienia symulacji:\nRozdzielczość = %d x %d\nLiczba cząstek = %d\n", width, height, count);
+    parseInputArgs(argc, argv, &width, &height, &count, &damping);
+    printf("Ustawienia symulacji:\nRozdzielczość = %d x %d\nLiczba cząstek = %d\nWsp. tłumienia = %f\n", width, height, count, damping);
 
     // Otworzenie okna i ustawienie kontekstu OpenGL
     GLFWwindow* window;
@@ -135,7 +139,7 @@ int main(int argc, char** argv)
         
         // Kinematyka cząstek
         updateAccelerations(&particles);
-        moveParticles(&particles, width, height, dt);
+        moveParticles(&particles, width, height, dt, damping);
 
         // Wyznaczanie FPS
         frames++;
@@ -162,18 +166,20 @@ int main(int argc, char** argv)
 }
 
 // Parsowanie argumentów
-void parseInputArgs(int argc, char** argv, int* width, int* height, int* count){
+void parseInputArgs(int argc, char** argv, int* width, int* height, int* count, double* damping){
     int option_index = 0;
     int c, arg;
+    double darg;
 
     static struct option long_options[] = {
         {"width",   required_argument, 0, 'w'},
         {"height",  required_argument, 0, 'h'},
         {"count",   required_argument, 0, 'c'},
+        {"damping", required_argument, 0, 'd'},
         {0, 0, 0, 0}
     };
 
-    while ((c = getopt_long(argc, argv, "w:h:c:", long_options, &option_index)) != -1) {
+    while ((c = getopt_long(argc, argv, "w:h:c:d:", long_options, &option_index)) != -1) {
         switch (c) {
             case 'w':
                 arg = atoi(optarg);
@@ -186,6 +192,10 @@ void parseInputArgs(int argc, char** argv, int* width, int* height, int* count){
             case 'c':
                 arg = atoi(optarg);
                 if (arg > 0) *count = arg;
+                break;
+            case 'd':
+                darg = atof(optarg);
+                if (arg >= 0.0) *damping = darg;
                 break;
             case '?':
                 break;
@@ -223,6 +233,8 @@ int setupWindowAndOpenGLContext(GLFWwindow** window, GLuint* texture, int width,
     glBindTexture(GL_TEXTURE_2D, *texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
     return 0;
@@ -321,11 +333,20 @@ void updateAccelerations(particles_t* particles){
 }
 
 // Zmiania prędkości i położenia cząstek na podstawie przyspieszeń
-void moveParticles(particles_t* particles, int width, int height, double dt){
+void moveParticles(particles_t* particles, int width, int height, double dt, double damping){
     for (int i = 0; i < particles->count; i++) {
+        // Opory ruchu
+        double drag = DEFAULT_DRAG; // smaller = less friction
+        particles->ax[i] -= drag * particles->vx[i];
+        particles->ay[i] -= drag * particles->vy[i];
+
         // Zmiana prędkości
         particles->vx[i] += particles->ax[i] * dt;
         particles->vy[i] += particles->ay[i] * dt;
+
+        // Tłumienie ruchu
+        particles->vx[i] *= damping;
+        particles->vy[i] *= damping;
 
         // Zmiana położenia
         particles->x[i] += particles->vx[i] * dt;
@@ -333,19 +354,23 @@ void moveParticles(particles_t* particles, int width, int height, double dt){
 
         // Odbicia
         if (particles->x[i] < 0 && particles->vx[i] < 0) { 
-            particles->x[i] = -particles->x[i]; 
+            // particles->x[i] = -particles->x[i]; 
+            particles->x[i] = 0;
             particles->vx[i] *= -1; 
         }
         if (particles->x[i] > width - 1 && particles->vx[i] > 0) { 
-            particles->x[i] = -particles->x[i] + 2*(width - 1); 
+            // particles->x[i] = -particles->x[i] + 2*(width - 1); 
+            particles->x[i] = width - 1; 
             particles->vx[i] *= -1; 
         }
         if (particles->y[i] < 0 && particles->vy[i] < 0) { 
-            particles->y[i] = -particles->y[i]; 
+            // particles->y[i] = -particles->y[i]; 
+            particles->y[i] = 0; 
             particles->vy[i] *= -1; 
         }
         if (particles->y[i] > height - 1 && particles->vy[i] > 0) { 
-            particles->y[i] = -particles->y[i] + 2*(height - 1);
+            // particles->y[i] = -particles->y[i] + 2*(height - 1);
+            particles->y[i] = height - 1;
             particles->vy[i] *= -1; 
         }
     }
